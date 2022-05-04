925710f51d63a5644a61ad0c4fdc3c270b241cc1
Yoon-Suji <jidl88@ewhain.net>
2022-05-04 23:56:20 +0900

    Feat: add Login function with redux

--- client/src/index.js
+++ client/src/index.js
@@ -9,6 +9,7 @@ import { applyMiddleware } from 'redux';
 import promiseMiddleware from 'redux-promise';
 import ReduxThunk from 'redux-thunk';
 import Reducer from './_reducers';
+import { createStore } from 'redux';
 
 const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
